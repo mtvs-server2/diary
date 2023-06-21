3fcb70a828a7e78456d58cc2a8a120bbd2a957ed
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-06-21 16:29:03 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/dirary/diaryproject/DiaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirary.diaryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
