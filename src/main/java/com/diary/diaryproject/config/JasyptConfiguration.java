e0e678bbca030d74179c37875ac327063693fe2e
sungjoon <myrhymetree@14.40.27.124>
2023-06-25 23:56:30 +0900

    fix : yaml파일 db pwd jasypt 암호화 적용

--- src/main/java/com/diary/diaryproject/config/JasyptConfiguration.java
+++ src/main/java/com/diary/diaryproject/config/JasyptConfiguration.java
@@ -1,6 +1,7 @@
 package com.diary.diaryproject.config;
 
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,15 +9,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.jasypt.encryption.StringEncryptor;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = "com.diary.diaryproject")
+@EnableEncryptableProperties
 public class JasyptConfiguration {
 
     @Value("${APP_EN}")
     private  String appEn;
 
     @Bean("jasyptStringEncryptor")
+    @Primary
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
@@ -24,6 +28,8 @@ public class JasyptConfiguration {
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setPoolSize(5);
         config.setPassword(appEn);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
         encryptor.setConfig(config);
 
         return encryptor;
