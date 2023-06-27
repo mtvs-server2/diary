99156de5b38920af3be72d1aca135cfd2de7a745
Ra Hyeon Ji <119282494+raxchaz@users.noreply.github.com>
2023-06-27 16:18:46 +0900

    test : login test

---
+++ src/test/java/com/diary/diaryproject/domain/service/LoginServiceTest.java
@@ -0,0 +1,28 @@
+package com.diary.diaryproject.domain.service;
+
+import com.diary.diaryproject.DiaryProjectApplication;
+import com.diary.diaryproject.config.BeanConfiguration;
+import com.diary.diaryproject.config.JpaConfiguration;
+import com.diary.diaryproject.domain.aggregate.entity.User;
+import com.diary.diaryproject.domain.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ContextConfiguration(classes = {JpaConfiguration.class, DiaryProjectApplication.class, BeanConfiguration.class})
+class LoginServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void login() {
+        User user = userRepository.findByIdAndPwd("mymy", "1234").get();
+
+        assertNotNull(user);
+    }
+}
