5ff3fe628114fdcdd22acab46028e922211271ca
hyojin lee <hodin030@gmail.com>
2023-06-28 19:33:09 +0900

    fix: 회원등록 및 아이디, 패스워드 유효성 검사 코드 수정

--- src/main/java/com/diary/diaryproject/domain/service/RegistMemberService.java
+++ src/main/java/com/diary/diaryproject/domain/service/RegistMemberService.java
@@ -5,13 +5,9 @@ import com.diary.diaryproject.domain.aggregate.entity.User;
 import com.diary.diaryproject.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
+import org.springframework.ui.Model;
 
 import javax.transaction.Transactional;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Component
 @Transactional
@@ -23,31 +19,14 @@ public class RegistMemberService {
         this.userRepository = userRepository;
     }
 
-    public static Map<String, String> validateHandling(Errors errors) {
-        Map<String, String> validatorResult = new HashMap<>();
-
-        for (FieldError error : errors.getFieldErrors()) {
-            String validKeyName = String.format("valid_%s", error.getField());
-            validatorResult.put(validKeyName, error.getDefaultMessage());
-        }
-        return validatorResult;
-    }
-
-    public void registUser(UserDTO userDTO) {
+    public void registUser(UserDTO userDTO, Model model) {
 
         User user = new User(userDTO.getId(), userDTO.getPwd(), userDTO.getNickName());
-        userRepository.save(user);
-    }
 
-    public boolean checkId(UserDTO userDTO) {
-        List<User> userlist=userRepository.findAll();
-        System.out.println("userlist = " + userlist);
-
-        for (int i=0; i<userlist.size(); i++){
-            if(userlist.get(i).getId().equals(userDTO.getId())){
-                return false;
-            }
+        if(!userDTO.getId().equals(null)
+        && !userDTO.getPwd().equals(null)
+        && !userDTO.getNickName().equals(null)) {
+            userRepository.save(user);
         }
-        return true;
     }
 }
