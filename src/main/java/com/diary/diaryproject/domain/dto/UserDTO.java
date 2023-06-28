ad8c6b248e6a4369ec54a9681fb0b466dc5998a2
hyojin lee <hodin030@gmail.com>
2023-06-28 11:09:22 +0900

    fix : 닉네임 값 안 넘어가는 오류, 비밀번호 재확인 안되는 오류 수정 안 쓰는 클래스 삭제(id,pwdCheckService)

--- src/main/java/com/diary/diaryproject/domain/dto/UserDTO.java
+++ src/main/java/com/diary/diaryproject/domain/dto/UserDTO.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Pattern;
 
 public class UserDTO {
 
-//    @NotEmpty(message = "아이디는 필수 입력값입니다.")
+//    @NotBlack(message = "아이디는 필수 입력값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,20}", message = "아이디를 5~20자리의 영문 대소문자, 숫자로 입력해주세요")
     private String id;
 
@@ -14,7 +14,7 @@ public class UserDTO {
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,20}", message = "비밀번호를 5~20자리의 영문 대소문자, 숫자로 입력해주세요")
     private String pwd;
 
-    @NotEmpty(message = "닉네임은 필수 입력값입니다.")
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String nickName;
 
 
