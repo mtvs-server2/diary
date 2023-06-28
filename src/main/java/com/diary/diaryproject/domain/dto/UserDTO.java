3216edb8cf75d4a8638c234c01fec7fae5d6e6eb
sjpark <myrhymetree@gmail.com>
2023-06-28 09:21:01 +0900

    fix : dto 추가

--- src/main/java/com/diary/diaryproject/domain/dto/UserDTO.java
+++ src/main/java/com/diary/diaryproject/domain/dto/UserDTO.java
@@ -2,16 +2,15 @@ package com.diary.diaryproject.domain.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public class UserDTO {
 
-//    @NotEmpty(message = "아이디는 필수 입력값입니다.")
+    @NotEmpty(message = "아이디는 필수 입력값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,20}", message = "아이디를 5~20자리의 영문 대소문자, 숫자로 입력해주세요")
     private String id;
 
-//    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{5,20}", message = "비밀번호를 5~20자리의 영문 대소문자, 숫자로 입력해주세요")
     private String pwd;
 
